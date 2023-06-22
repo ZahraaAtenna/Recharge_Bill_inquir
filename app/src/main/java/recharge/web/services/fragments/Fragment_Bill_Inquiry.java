de60a2c0dac85531ae17aa2e3bdeba26a9202b08
alinarooi <alinarooi45@gmail.com>
2023-06-23 01:19:43 +0430

    برنامه خرید شارژ و استعلام قبوض

--- app/src/main/java/recharge/web/services/fragments/Fragment_Bill_Inquiry.java
+++ app/src/main/java/recharge/web/services/fragments/Fragment_Bill_Inquiry.java
@@ -52,7 +52,7 @@ public class Fragment_Bill_Inquiry extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment__bill__inquiry, container, false);
     }
 }
